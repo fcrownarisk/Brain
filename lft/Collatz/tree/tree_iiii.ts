b309a3cd4be03812baf2aa0ddfa911fbc8128688
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-07-21 14:53:14 +0800

    Update tree_iiii.ts

--- lft/Collatz/tree/tree_iiii.ts
+++ lft/Collatz/tree/tree_iiii.ts
@@ -2,14 +2,14 @@ import './tree_i'
 import './tree_ii'
 import './tree_iii'
 void function tree_iiii(){
-    void 'tree_i + tree_ii'
-    void 'tree_i - tree_ii'
-    void 'tree_i * tree_ii'
-    void 'tree_i % tree_ii'
+    return 'tree_i + tree_ii'
+    return 'tree_i - tree_ii'
+    return 'tree_i * tree_ii'
+    return 'tree_i % tree_ii'
 
-    void 'tree_ii + tree_iii'
-    void 'tree_ii - tree_iii'
-    void 'tree_ii * tree_iii'
-    void 'tree_ii % tree_iii'
+    return 'tree_ii + tree_iii'
+    return 'tree_ii - tree_iii'
+    return 'tree_ii * tree_iii'
+    return 'tree_ii % tree_iii'
     return tree_iiii
 }
