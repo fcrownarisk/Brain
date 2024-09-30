c0f6f881a8bca9eb0f9b8a3f14487a6e96bf253f
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-30 12:04:14 +0100

    Update rstnumbertostring.ts

--- lft/Letter/opqrst/rstnumbertostring.ts
+++ lft/Letter/opqrst/rstnumbertostring.ts
@@ -1,7 +1,4 @@
 import './opqrst/rst'
-declare module x{}
-declare module y{}
-declare module z{}
 void function rst(r:number,s:number,String,t:String){
         void 'r:Path2D'
         void 's:PerformanceResourceTiming'
